9acef844634b2c09bf26087d924b6e02e285b87a
jiangeng <jiangpeng@agilean.cn>
2020-07-22 18:25:25 +0800

    spring-cloud-cipher

---
+++ src/main/java/com/study/springcloud/cipher/Application.java
@@ -0,0 +1,24 @@
+package com.study.springcloud.cipher;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class Application {
+	@RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
